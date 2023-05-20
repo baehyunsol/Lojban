42fef3f46f4fa193de4bafea53dcaf6bedb4385b
baehyunsol <baehyunsol@github.com>
2023-05-20 19:01:04 +0900

    fix margin issues

--- nav.js
+++ nav.js
@@ -14,7 +14,7 @@ document.getElementById("settingsopenbutton").addEventListener("click", open_set
 document.getElementById("settingsclosebutton").addEventListener("click", close_settings);
 
 var root_doc = document.querySelector(":root");
-var horizontal_padding = 96;
+var horizontal_padding = 4;
 
 function grow_horiz() {
     horizontal_padding -= 24;
@@ -39,8 +39,8 @@ function shrink_horiz() {
 document.getElementById("growhorizontalbutton").addEventListener("click", grow_horiz);
 document.getElementById("shrinkhorizontalbutton").addEventListener("click", shrink_horiz);
 
-var font_size_landscape = 21;
-var font_size_portrait = 16;
+var font_size_landscape = 19;
+var font_size_portrait = 14;
 
 function shrink_font() {
     font_size_landscape -= 1;
